ac9acb22e8a81667c425e0832010f9f53d99b2e9
Maurice <ombewamaurice79@gmail.com>
2025-11-05 13:36:16 +0300

    Update app routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { App, Body, Card } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: App },
+    { path: 'about', component:Card },
+    { path: 'blogs', component: Body }
+];
