1d2a36bdf200192cf09cb7f15f779e9662227196
Maurice <ombewamaurice79@gmail.com>
2025-11-05 13:46:37 +0300

    Update routes with component mappings for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { App, Body, Card } from './app';
 
 export const routes: Routes = [
+    { path: '', component:App },
     { path: 'home', component: App },
     { path: 'about', component:Card },
-    { path: 'blogs', component: Body }
+    { path: 'blogs', component: Body },
+    { path: '**', redirectTo: ''}
 ];
