4883596f0d3d756211dca07feca4fcef4e71a5f2
Maurice <ombewamaurice79@gmail.com>
2025-11-05 09:32:58 +0300

    Add Header component with template and style configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-
+//header section
+@Component({
+  selector: 'header',
+  templateUrl: 'app.header.html',
+  styleUrl: './app.header.css'
+})
+export class Header{
+  protected readonly title = signal('day3');
+}
 
 @Component({
   selector: 'app-root',
