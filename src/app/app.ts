a432e0d128fb1cf2a1d98d94296624e48d384d72
Maurice <ombewamaurice79@gmail.com>
2025-11-12 10:37:49 +0300

    Add post creation functionality with form validation and API integration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 //header section
@@ -25,8 +26,8 @@ export class Card{
 
 //body section
 @Component({
-  selector: 'body',
-  imports: [CommonModule],
+  selector: 'app-body',
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.body.html',
   styleUrl: './app.body.css'
 })
@@ -36,6 +37,10 @@ export class Body implements OnInit {
   protected posts: any[] = [];
   protected loading = false;
   protected error = '';
+  protected textColor = 'green';
+  protected newPost = { title: '', body: '' };
+  protected isSubmitting = false;
+  protected sample: any;
 
   constructor(private http: HttpClient) {}
 
@@ -47,7 +52,7 @@ export class Body implements OnInit {
     this.loading = true;
     this.error = '';
     
-    this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts?_limit=3')
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts?_limit=7')
       .subscribe({
         next: (data) => {
           this.posts = data;
@@ -60,6 +65,36 @@ export class Body implements OnInit {
           console.error('Error:', err);
         }
       });
+  }
+
+  addPost() {
+    if (this.newPost.title.trim()== '' || this.newPost.body.trim() == '') {
+      this.error = 'Please fill in both title and body';
+      return;
+    }
+
+
+    this.isSubmitting = true;
+    this.error = '';
+
+    this.http.post<any>('https://jsonplaceholder.typicode.com/posts', {
+      title: this.newPost.title,
+      body: this.newPost.body,
+      userId: 1
+    }).subscribe({
+      next: (data) => {
+        this.sample = data;
+        this.posts.unshift(data);
+        this.newPost = { title: '', body: '' };
+        this.isSubmitting = false;
+        console.log('Post added:', data);
+      },
+      error: (err) => {
+        this.error = 'Failed to add post';
+        this.isSubmitting = false;
+        console.error('Error:', err);
+      }
+    });
   }
 }
 
